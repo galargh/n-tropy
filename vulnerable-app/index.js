f38a6f84451499143a999a8f912fae9d0757f1c1
Piotr Galar <fjalar.galar@gmail.com>
2016-05-12 21:51:36 +0200

    Fix cors

--- vulnerable-app/index.js
+++ vulnerable-app/index.js
@@ -5,14 +5,13 @@ var cors = require('./helpers/cors')
 var app = express()
 
 app.use(bodyParser.json())
-app.use(checkAccept)
 app.use(cors)
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/static/index.html')
 })
-app.post('/secret', require('./helpers/check'), require('./controllers/get'))
-app.post('/password', require('./controllers/set'))
+app.post('/secret', checkAccept, require('./helpers/check'), require('./controllers/get'))
+app.post('/password', checkAccept, require('./controllers/set'))
 
 app.listen(8888, function() {
     console.log('Try and break me on port 8888!')
