e4fba1ab33a09b11468b6626d1bbf1fac36c812f
Piotr Galar <fjalar.galar@gmail.com>
2016-05-12 21:16:37 +0200

    Added UI for vulnerable app

--- vulnerable-app/index.js
+++ vulnerable-app/index.js
@@ -1,11 +1,16 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var checkAccept = require('./helpers/accept')
+var cors = require('./helpers/cors')
 var app = express()
 
 app.use(bodyParser.json())
 app.use(checkAccept)
+app.use(cors)
 
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/static/index.html')
+})
 app.post('/secret', require('./helpers/check'), require('./controllers/get'))
 app.post('/password', require('./controllers/set'))
 
